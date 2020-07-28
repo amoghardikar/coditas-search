ae1c5942ef36883d81c7337faabcffa4eb9191a3
Amogh Hardikar <hardiamogh@gmail.com>
2020-07-28 21:10:26 +0530

    printed output in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { SearchService } from './search.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coditas-search-assignment';
+  results: Object;
+  searchTerm$ = new Subject<string>();
+  constructor(private searchService: SearchService) {
+    this.searchService.search(this.searchTerm$)
+      .subscribe(results => {
+        console.log(results)
+        //this.results = results['results'];
+        //console.log(this.results)
+      });
+  }
+
 }
---
+++ src/app/search.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from  "@angular/common/http";
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import { debounceTime,  } from 'rxjs/operators';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';;
+
+@Injectable()
+export class SearchService {
+  baseUrl: string = 'https://api.github.com/search/users?q=';
+  queryUrl: string = '?search=';
+
+  constructor(private http: HttpClient) { }
+
+  search(terms: Observable<string>) {
+    // return terms.debounceTime(100)
+    //   .distinctUntilChanged()
+    //   .switchMap(term => this.searchEntries(term));
+    return  terms
+  .pipe(debounceTime(800),
+        distinctUntilChanged(),
+        switchMap(term => this.searchEntries(term)));
+  }
+
+  searchEntries(term) {
+    return this.http
+        .get(this.baseUrl+ term)
+  }
+}
